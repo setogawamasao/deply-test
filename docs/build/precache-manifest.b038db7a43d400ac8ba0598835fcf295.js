c362be44dd0db4e46e87107df1efae938968aca0
masaosetogawa <ms.job.hunting@gmail.com>
2020-10-14 22:13:20 +0900

    Add view port

---
+++ docs/build/precache-manifest.b038db7a43d400ac8ba0598835fcf295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61098918ae9b2b8f09d032a3ece7fe0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "17d909131d165b4e5f7f",
+    "url": "./static/css/main.186ae262.chunk.css"
+  },
+  {
+    "revision": "3af8002ebd5847e0cfd2",
+    "url": "./static/js/2.79a06790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.79a06790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17d909131d165b4e5f7f",
+    "url": "./static/js/main.6c937d48.chunk.js"
+  },
+  {
+    "revision": "7ec6f4cbc143c96f961f",
+    "url": "./static/js/runtime-main.170eb1ab.js"
+  }
+]);
